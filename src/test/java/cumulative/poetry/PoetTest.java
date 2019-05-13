b1bbb4ddf038a66c512c232a3bbd56955a55de27
shivam.rungta <shivam.rungta@10.132.20.254>
2019-05-13 11:32:46 +0530

    Added Test Cases

--- src/test/java/cumulative/poetry/PoetTest.java
+++ src/test/java/cumulative/poetry/PoetTest.java
@@ -11,4 +11,20 @@ public class PoetTest {
         Poet poet = new Poet();
         assertFalse("Poet should say something, but said nothing",poet.recite().isEmpty());
     }
+    
+    @Test public void shouldSayPoem() {
+    	Poet poet = new Poet();
+    	String actual = poet.recite();
+		assertEquals(
+				"Day 1 - \nThis is the house that Jack built.\n\nDay 2 - \nThis is the malth that lay in\nthe house that Jack built.\n\nDay 3 - \nThis is the rat that ate\nthe malth that lay in\nthe house that Jack built.\n\nDay 4 - \nThis is the cat that killed\nthe rat that ate\nthe malth that lay in\nthe house that Jack built.\n\nDay 5 - \nThis is the dog that worried\nthe cat that killed\nthe rat that ate\nthe malth that lay in\nthe house that Jack built.\n\nDay 6 - \nThis is that cow with the crumpled horn that tossed\nthe dog that worried\nthe cat that killed\nthe rat that ate\nthe malth that lay in\nthe house that Jack built.\n\nDay 7 - \nThis is the maiden all forlorn that milked\nthat cow with the crumpled horn that tossed\nthe dog that worried\nthe cat that killed\nthe rat that ate\nthe malth that lay in\nthe house that Jack built.\n\nDay 8 - \nThis is the man all tattered and torn that kissed\nthe maiden all forlorn that milked\nthat cow with the crumpled horn that tossed\nthe dog that worried\nthe cat that killed\nthe rat that ate\nthe malth that lay in\nthe house that Jack built.\n\nDay 9 - \nThis is the priest all shaven and shorn that married\nthe man all tattered and torn that kissed\nthe maiden all forlorn that milked\nthat cow with the crumpled horn that tossed\nthe dog that worried\nthe cat that killed\nthe rat that ate\nthe malth that lay in\nthe house that Jack built.\n\nDay 10 - \nThis is the rooster that crowed in the morn that woke\nthe priest all shaven and shorn that married\nthe man all tattered and torn that kissed\nthe maiden all forlorn that milked\nthat cow with the crumpled horn that tossed\nthe dog that worried\nthe cat that killed\nthe rat that ate\nthe malth that lay in\nthe house that Jack built.\n\nDay 11 - \nThis is the farmer sowing his corn that kept\nthe rooster that crowed in the morn that woke\nthe priest all shaven and shorn that married\nthe man all tattered and torn that kissed\nthe maiden all forlorn that milked\nthat cow with the crumpled horn that tossed\nthe dog that worried\nthe cat that killed\nthe rat that ate\nthe malth that lay in\nthe house that Jack built.\n\nDay 12 - \nThis is the horse and the hound and the horn that belonged to\nthe farmer sowing his corn that kept\nthe rooster that crowed in the morn that woke\nthe priest all shaven and shorn that married\nthe man all tattered and torn that kissed\nthe maiden all forlorn that milked\nthat cow with the crumpled horn that tossed\nthe dog that worried\nthe cat that killed\nthe rat that ate\nthe malth that lay in\nthe house that Jack built."
+					 , 
+					 actual);
+    	
+    }
+    
+    @Test public void shouldRevealForDay() {
+    	Poet poet = new Poet();
+    	String actual = poet.revealforday(2);
+		assertEquals("This is the malth that lay in\nthe house that Jack built.", actual);
+    }
 }
