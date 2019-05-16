97fa7bc44f8e5c55e848129b702b9af985ad65ef
shivam.rungta <shivam.rungta@10.132.20.254>
2019-05-16 12:19:01 +0530

    Options Class Simplified

--- src/main/java/cumulative/poetry/Options.java
+++ src/main/java/cumulative/poetry/Options.java
@@ -7,26 +7,19 @@ public class Options {
 	
 	//constants
 	public static final int OPTION_PRESENT_VALUE_DEFAULT = 1;
-	public static final int OPTION_ABSENT_VALUE_DEFAULT = 0;
+	public static final String REGEX_TO_CHECK_NUMBER = "^[0-9]+?";
 	
 	HashMap<String, Integer> options = new HashMap<String, Integer>();
 	
 	//initialise options with their values
 	Options(String[] args) {
-		for(int i = 0;i < args.length;i++) {
-			if(args[i].startsWith("--")) {
-				boolean flag = true;
-				try {			
-					Integer.parseInt(args[i+1]);	
-				}
-				catch(Exception e){
-					flag = false;
-				}
-				if(flag) { 
-					options.put(args[i], Integer.parseInt(args[i+1])); 
+		for(int option_index = 0;option_index < args.length; option_index++) {
+			if(args[option_index].startsWith("--")) {
+				if(args[option_index+1].matches(REGEX_TO_CHECK_NUMBER)) { 
+					options.put(args[option_index], Integer.parseInt(args[option_index+1])); 
 				} 
 				else {
-					options.put(args[i], OPTION_PRESENT_VALUE_DEFAULT);
+					options.put(args[option_index], OPTION_PRESENT_VALUE_DEFAULT);
 				}
 			}
 		}
@@ -37,18 +30,6 @@ public class Options {
 		return options;
 	}
 	
-	//method to return value of options
-	public int getOptionValue(String option) {
-		int value;
-		
-		if(options.containsKey(option)) {
-			value = options.get(option);
-		}
-		else {
-			value = OPTION_ABSENT_VALUE_DEFAULT;
-		}
-		
-		return value;
-	}
+	
 
 }
